01d9fadb853e73c1d05f3b242096f67d7a1c4459
Joran Schols <schols.joran@gmail.com>
2024-12-21 11:33:21 +0100

    hub endpoints

--- api/Hub/SessionHub.cs
+++ api/Hub/SessionHub.cs
@@ -1,6 +1,5 @@
 ﻿
 using Microsoft.AspNetCore.SignalR;
-using System.Collections.Concurrent;
 
 public class SessionHub : Hub
 {
@@ -18,5 +17,15 @@ public class SessionHub : Hub
     public async Task UpdateSession(string sessionId)
     {
         await Clients.Group(sessionId).SendAsync("ReceiveScoreUpdate", sessionId);
+    }
+
+    public async Task SetEmdrState(string sessionId, string state)
+    {
+        await Clients.Group(sessionId).SendAsync("RecieveEmdrState", state);
+    }
+
+    public async Task SetSpeed(string sessionId, int speed)
+    {
+        await Clients.Group(sessionId).SendAsync("RecieveSpeed", speed);
     }
 }
